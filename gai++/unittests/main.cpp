3d2d11bb035e36965d18b2714fbd44d2f5adad15
Nathan Holmberg <nathan.holmberg@gmail.com>
2013-03-10 21:32:28 +1300

    * unittest structure

---
+++ gai++/unittests/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  unittests
+//
+//  Created by Nathan Holmberg on 10/03/13.
+//  Copyright (c) 2013 nshgraph. All rights reserved.
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();}
+
---
+++ gai++/unittests/test_datastoresqlite.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  unittests
+//
+//  Created by Nathan Holmberg on 10/03/13.
+//  Copyright (c) 2013 nshgraph. All rights reserved.
+//
+
+#include "gtest/gtest.h"
+
+#include "DataStoreSqlite.h"
+
+TEST (DataStoreSqlLite, properties)
+{
+    
+}
