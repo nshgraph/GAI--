fed5d5b2aabb7e32abe157e9575d09ef6660ae5c
Steve Hosking <steviehosking@gmail.com>
2013-03-29 18:36:07 +1300

    Added comments to Dispatcher.

--- gai++/src/Dispatcher.cpp
+++ gai++/src/Dispatcher.cpp
@@ -17,11 +17,26 @@ namespace GAI
     mDataStore( data_store ),
 	mDispatchEvent( NULL ),
 	mDispatchEventBase( NULL )
+    ///
+    /// Constructor
+    ///
+    /// @param data_store
+	///		DataStore object reference for dispatcher to store hits
+    ///
+    /// @param opt_out
+	///		Whether Google Analytics tracking is enabled/disabled
+    ///
+    /// @param dispatch_interval
+    ///     Time (in seconds) that each dispatch will occur
+    ///
 	{
 		createTimerThread();
 	}
 	
 	Dispatcher::~Dispatcher()
+    ///
+    /// Destructor
+    ///
 	{
 		if( mDispatchEvent )
 		{
@@ -38,6 +53,15 @@ namespace GAI
 	}
     
     bool Dispatcher::storeHit( const Hit& hit )
+    ///
+    /// Stores a Hit in the DataStore
+    ///
+    /// @param hit
+    ///     The Hit object to store
+    ///
+    /// @return
+    ///     Whether the operation was successful
+    ///
     {
 		if( mbOptOut )
 			return true;
@@ -49,32 +73,75 @@ namespace GAI
     }
 	
 	void Dispatcher::queueDispatch()
-    /// really: send outstanding hits if possible
+    ///
+    /// Send all pending Hits in the DataStore
+    ///
+    /// @return
+    ///     Nothing
+    ///
 	{
 		throw "Not yet implemented";
 	}
 	
 	void Dispatcher::cancelDispatch()
+    ///
+    /// Cancel the current dispatch.
+    ///
+    /// @return
+    ///     Nothing
+    ///
 	{
 		throw "Not yet implemented";
 	}
 	
 	bool Dispatcher::isOptOut() const
+    ///
+    /// Return whether Google Analytics tracking is enabled/disabled
+    ///
+    /// @return
+    ///     Tracking is enabled/disabled
+    ///
 	{
 		return mbOptOut;
 	}
 	
 	void Dispatcher::setOptOut( const bool opt_out )
+    ///
+    /// Set whether Google Analytics tracking is enabled/disabled.
+	/// If opt_out is true, Hits will not be stored in the DataStore.
+    ///
+    /// @param opt_out
+    ///     The enabled/disable boolean
+    ///
+    /// @return
+    ///     Nothing
+    ///
 	{
 		mbOptOut = opt_out;
 	}
 	
 	int Dispatcher::getDispatchInterval() const
+    ///
+    /// Return the dispatch interval.
+    ///
+    /// @return
+    ///     The dispatch interval
+    ///
 	{
 		return mDispatchInterval;
 	}
 	
 	void Dispatcher::setDispatchInterval( const double dispatch_interval )
+    ///
+    /// Set the dispatch interval. Stored Hits are attempted to be sent to
+	/// Google Analytics each time this interval expires.
+	///
+    /// @param dispatch_interval
+    ///     The dispatch interval in seconds
+    ///
+    /// @return
+    ///     Nothing
+    ///
 	{
 		mDispatchInterval = dispatch_interval;
 		
@@ -88,11 +155,27 @@ namespace GAI
 	}
 	
 	void Dispatcher::createTimerThread()
+	///
+	/// Creates a thread and passed this object to it.
+	///
+    /// @return
+    ///     Nothing
+    ///
 	{
 		pthread_create( &mTimerThread, NULL, &Dispatcher::timerThread, this );
 	}
 	
 	void* Dispatcher::timerThread( void* context )
+	///
+	/// Thread which enters an event loop that will trigger a callback after
+	/// each dispatch interval.
+	///
+    /// @param context
+    ///     A void* (the Dispatcher) which will be passed to the callback
+	///
+    /// @return
+    ///     Nothing
+    ///
 	{
 		Dispatcher *dispatcher = static_cast<Dispatcher*>( context );
 		
@@ -108,6 +191,20 @@ namespace GAI
 	}
 	
 	void Dispatcher::timerCallback( evutil_socket_t file_descriptor, short events, void* context )
+	///
+	/// The callback after each dispatch interval has passed. The context will trigger
+	/// a queueDispatch on the context (the Dispatcher).
+	///
+    /// @param file_descriptor
+	///
+    /// @param events
+	///
+    /// @param context
+    ///     A void* (the Dispatcher) passed to the callback
+	///
+    /// @return
+    ///     Nothing
+    ///
 	{
 		Dispatcher *dispatcher = static_cast<Dispatcher*>( context );
 		
