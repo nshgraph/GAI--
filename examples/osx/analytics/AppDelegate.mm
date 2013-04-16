a3026731d2a9b0933f078953e891ea2c3210ba1c
Steve Hosking <steviehosking@gmail.com>
2013-04-16 21:21:03 +1200

    * OSX example now runs with GAI lib

--- examples/osx/analytics/AppDelegate.mm
+++ examples/osx/analytics/AppDelegate.mm
@@ -5,11 +5,15 @@
 
 #import "AppDelegate.h"
 
+#include "GAI.h"
+
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
 	// Insert code here to initialize your application
+	
+	GAI::Analytics::sharedInstance( "Analytics Example App", "./" );
 }
 
 @end
